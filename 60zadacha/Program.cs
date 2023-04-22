e322433285b7a54cd8c754569cc68d11af98f2ae
Valeriy Ziborov <ziboroffvalera@gmail.com>
2023-04-22 11:40:55 +0300

    Initial commit

---
+++ 60zadacha/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Start();
+void Start()
+{
+    int rows = SetNumber("строк");
+    int columns = SetNumber("колонок");
+    int pages = SetNumber("массивов");
+    int[,,] matrix = FillArray(rows, columns, pages);
+    PrintIndex(matrix);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+int SetNumber(string text = "")
+{
+    Console.Write($"Введите количество {text}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+int[,,] FillArray(int rows, int columns, int pages)
+{
+    int[,,] array = new int[rows, columns, pages];
+    int count = 10;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < pages; k++)
+            {
+                array[k, i, j] = count;
+                count += 2;
+            }
+        }
+    }
+    return array;
+}
+
+void PrintIndex(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
---
+++ 62zadacha/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив . Размер вводит юзер
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.WriteLine($"Введите размер массива n x n:");
+int n = InputNumbers("Введите n: ");
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+int[,] spiralMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= spiralMatrix.GetLength(0) * spiralMatrix.GetLength(1))
+{
+  spiralMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < spiralMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= spiralMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > spiralMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(spiralMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
